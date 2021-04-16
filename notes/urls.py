daefe1dee6c780ee3745b2d4390b82e520fdb389
jurrutiag <juan.urrutia.gandolfo@gmail.com>
2021-04-16 16:41:36 -0400

    Changed settings for deployment

--- notes/urls.py
+++ notes/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('notetaking.urls')),
     path('admin/', admin.site.urls),
-    path('notetaking/', include('notetaking.urls')),
     path('registration/', include('users.urls')),
 ]
